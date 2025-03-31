5d8e9c61542a4381d47491abafa5719233f6a595
rodriquelali <rodriquelali@gmail.com>
2025-03-30 21:09:12 -0400

    Add constralayouts.

--- linearLayout/app/src/main/java/com/example/linearlayout/MainActivity.kt
+++ linearLayout/app/src/main/java/com/example/linearlayout/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.linearlayout
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun llamarHome(view: View){
+        val i = Intent(this, home::class.java)
+        startActivity(i)
     }
 }
