66d41aa182fcb62ee6d1f84cc1f8f636ba831f00
rodriquelali <rodriquelali@gmail.com>
2025-05-02 12:21:37 -0400

    Add constructor.

--- sumaPoo/app/src/main/java/com/example/sumapoo/MainActivity.kt
+++ sumaPoo/app/src/main/java/com/example/sumapoo/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sumapoo.data.Calculadora
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +29,8 @@ class MainActivity : AppCompatActivity() {
         val btnOperar = findViewById<Button>(R.id.btnSuma)
 
         btnOperar.setOnClickListener{
-            val n1 = et1.text.toString().toInt()
-            val n2 = et2.text.toString().toInt()
-            val total = n1 + n2
-            tvR.text = "La suam es: ${total.toString()}"
+            val calUno:Calculadora = Calculadora(et1.text.toString().toFloat(), et2.text.toString().toFloat())
+            tvR.text = "La suam es: ${calUno.resultado.toString()}"
         }
 
     }
---
+++ sumaPoo/app/src/main/java/com/example/sumapoo/data/Calculadora.kt
@@ -0,0 +1,38 @@
+package com.example.sumapoo.data
+
+class Calculadora  {
+    //atributos
+    var resultado: Float
+
+    constructor(numeroUno: Int, numeroDos:Int){
+        resultado = (numeroDos + numeroUno).toFloat()
+    }
+
+    constructor(numeroUno: Float, numeroDos: Float)  {
+        resultado = (numeroDos + numeroUno)
+    }
+
+
+
+
+
+    /*
+    private Int numeroUno;
+    private Int numeroDos;
+    private Int resultado;
+
+    function Calculadora(numeroUno: Int, numeroDos:Int, resultado:Int){
+       this.numeroUno = numeroUno
+        this.numeroDos = numeroDos
+        this.resultado = resultado
+    }
+
+    funtion int sumar(){
+
+    }
+    */
+
+    //metodos
+
+
+}
