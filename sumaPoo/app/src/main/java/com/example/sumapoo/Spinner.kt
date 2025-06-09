36c265e193534e4fae27ca3a7cffdfe8f4b4858e
rodriquelali <rodriquelali@gmail.com>
2025-06-09 12:56:51 -0400

    Add getItemListener spinner.

--- sumaPoo/app/src/main/java/com/example/sumapoo/Spinner.kt
+++ sumaPoo/app/src/main/java/com/example/sumapoo/Spinner.kt
@@ -1,12 +1,16 @@
 package com.example.sumapoo
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Spinner
+import android.widget.Toast
 
 class Spinner : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +28,23 @@ class Spinner : AppCompatActivity() {
         val lista:MutableList<String> = mutableListOf("sumar", "restar", "multiplicar", "dividir", "Raiz cuadrada")
         val adap = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lista)
         spin.adapter = adap
+
+        spin.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+                Toast.makeText(this@Spinner, "$selectedItem", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
 
     }
 }
